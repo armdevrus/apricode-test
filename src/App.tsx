dd7ec7d8d9f4b81ebd4dfb9348bb902d8a51910a
Arman <aisenovdev@gmail.com>
2022-11-19 20:00:14 +0500

    add mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,59 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Form from './components/Form/Form';
+import { useState, useEffect } from "react";
+import Form from "./components/Form/Form";
+import { observer } from "mobx-react-lite";
+import "./App.css";
 
 export type Todo = {
   id: number;
   title: string;
   complete: boolean;
-}
+};
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
-  const url = 'http://localhost:4000/todos/';
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const url = "http://localhost:4000/todos/";
 
   useEffect(() => {
     const fetchData = () => {
       fetch(url)
-        .then(res => res.json())
-        .then(data => setTodos(data))
-        .catch(err => console.log(err.message)
-        )
-    }
-    fetchData()
-  }, [url])
+        .then((res) => res.json())
+        .then((data) => setTodos(data))
+        .catch((err) => console.log(err.message));
+    };
+    fetchData();
+  }, [url]);
 
-  const addTodo = (todo: { title: string, complete: boolean }) => {
+  const addTodo = (todo: { title: string; complete: boolean }) => {
     const fetchData = () => {
       fetch(url, {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(todo)
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(todo),
       })
-        .then(res => res.json())
-        .then(data => setTodos([...todos, data]))
-        .catch(err => console.log(err.message)
-        )
-    }
-    fetchData()
-  }
+        .then((res) => res.json())
+        .then((data) => setTodos([...todos, data]))
+        .catch((err) => console.log(err.message));
+    };
+    fetchData();
+  };
   const deleteTodo = (id: number) => {
     const fetchData = () => {
       fetch(url + id, {
-        method: 'DELETE',
+        method: "DELETE",
       })
         .then(() => {
-          setTodos(todos.filter(todo => todo.id !== id))
+          setTodos(todos.filter((todo) => todo.id !== id));
         })
-        .catch(err => console.log(err.message)
-        )
-    }
-    fetchData()
-  }
+        .catch((err) => console.log(err.message));
+    };
+    fetchData();
+  };
 
   return (
     <>
-      <Form addTodo={addTodo}/>
+      <Form addTodo={addTodo} />
       <ul className="App">
-        {todos.map(todo => (
+        {todos.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
@@ -63,4 +61,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
